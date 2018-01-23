578888c542e2daff713e8a9a45a157ccb74a4e0a
linton AI <gezhonglin@ainirobot.com>
2018-01-23 11:09:44 +0800

    up

---
+++ data/conf/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => 'localhost',
+    // 数据库名
+    'database' => 'cms',
+    // 用户名
+    'username' => 'root',
+    // 密码
+    'password' => '123456',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8',
+    // 数据库表前缀
+    'prefix'   => 'cms_',
+    "authcode" => '401DeXJFGITSvQJvDR',
+    //#COOKIE_PREFIX#
+];
