90da36ad354d1fcad503a1c706d095a6e01eef0e
linton AI <gezhonglin@ainirobot.com>
2019-03-18 18:01:11 +0800

    1

--- data/conf/database.php
+++ data/conf/database.php
@@ -7,13 +7,13 @@ return [
     // 数据库类型
     'type'     => 'mysql',
     // 服务器地址
-    'hostname' => 'localhost',
+    'hostname' => '127.0.0.1',
     // 数据库名
     'database' => 'cms',
     // 用户名
     'username' => 'root',
     // 密码
-    'password' => '123456',
+    'password' => 'Sohpeter123',
     // 端口
     'hostport' => '3306',
     // 数据库编码默认采用utf8
